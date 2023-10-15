ad09c33c2eedc880184ff850092fdaaf3dca5d02
LaM0uette <LaM0uette.dev@gmail.com>
2023-10-15 15:38:00 +0200

    show all products on main page

--- StudiBloc3_Mercadona.WebApp/Program.cs
+++ StudiBloc3_Mercadona.WebApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StudiBloc3_Mercadona.WebApp.Components;
+using StudiBloc3_Mercadona.WebApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri("https://localhost:7173")});
+builder.Services.AddScoped<ApiProductService>();
 
 await builder.Build().RunAsync();
---
+++ StudiBloc3_Mercadona.WebApp/Services/ApiProductService.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using StudiBloc3_Mercadona.Model;
+
+namespace StudiBloc3_Mercadona.WebApp.Services;
+
+public class ApiProductService
+{
+    private readonly HttpClient _httpClient;
+    
+    public ApiProductService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<IEnumerable<Product>> GetAllProductsAsync()
+    {
+        var response = await _httpClient.GetAsync("/api/Product/Product/GetAll");
+        response.EnsureSuccessStatusCode();
+        var products = await response.Content.ReadFromJsonAsync<IEnumerable<Product>>();
+        return products;
+    }
+}
