ae55aa91c478548e7965a96bac945d16237e3b6c
LaM0uette <LaM0uette.dev@gmail.com>
2023-10-15 21:00:53 +0200

    refactor api and model

--- StudiBloc3_Mercadona.Api/Core/Repository/IRepository.cs
+++ StudiBloc3_Mercadona.Api/Core/Repository/IRepository.cs
@@ -3,9 +3,6 @@
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
-        Task<T> GetByIdAsync(int id);
         Task AddAsync(T entity);
-        Task UpdateAsync(T entity);
-        Task DeleteAsync(T entity);
     }
 }
--- StudiBloc3_Mercadona.Model/Product.cs
+++ StudiBloc3_Mercadona.Model/Product.cs
@@ -11,7 +11,4 @@ public class Product
     public string? Description { get; set; }
     public float Price { get; set; }
     public string? Image { get; set; }
-    
-    //[ForeignKey("CategoryId")]
-    //public Category Category { get; set; }
 }
