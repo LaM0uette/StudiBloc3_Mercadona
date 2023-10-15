d653684e7b1368a7a0a73ca37df82899d224b8ad
LaM0uette <LaM0uette.dev@gmail.com>
2023-10-15 10:38:35 +0200

    add ProductServices

---
+++ StudiBloc3_Mercadona/Core/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using StudiBloc3_Mercadona.Model;
+
+namespace StudiBloc3_Mercadona.Core.Repository;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAllProductsAsync();
+    Task<Product> GetProductByIdAsync(int id);
+    Task AddProductAsync(Product product);
+    Task UpdateProductAsync(Product product);
+    Task DeleteProductAsync(Product product);
+}
---
+++ StudiBloc3_Mercadona/Core/Services/ProductServices.cs
@@ -0,0 +1,47 @@
+﻿using StudiBloc3_Mercadona.Core.Repository;
+using StudiBloc3_Mercadona.Model;
+
+namespace StudiBloc3_Mercadona.Core.Services;
+
+public class ProductServices : IProductRepository
+{
+    #region Statements
+
+    private readonly IRepository<Product> _productRepository;
+
+    public ProductServices(IRepository<Product> productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    #endregion
+
+    #region Tasks
+
+    public async Task<IEnumerable<Product>> GetAllProductsAsync()
+    {
+        return await _productRepository.GetAllAsync();
+    }
+
+    public async Task<Product> GetProductByIdAsync(int id)
+    {
+        return await _productRepository.GetByIdAsync(id) ?? throw new Exception();
+    }
+
+    public async Task AddProductAsync(Product product)
+    {
+        await _productRepository.AddAsync(product);
+    }
+
+    public async Task UpdateProductAsync(Product product)
+    {
+        await _productRepository.UpdateAsync(product);
+    }
+
+    public async Task DeleteProductAsync(Product product)
+    {
+        await _productRepository.DeleteAsync(product);
+    }
+
+    #endregion
+}
