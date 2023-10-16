43a16bacc3580aedce0df0ca269883714bf67e51
LaM0uette <LaM0uette.dev@gmail.com>
2023-10-16 17:41:39 +0200

    fix DisplayPriceWithPromotion calcul

--- StudiBloc3_Mercadona.Model/FullProduct.cs
+++ StudiBloc3_Mercadona.Model/FullProduct.cs
@@ -6,4 +6,14 @@ public class FullProduct
     public Promotion Promotion { get; set; }
     public ProductPromotion ProductPromotion { get; set; }
     public Category Category { get; set; }
+    
+    public string DisplayPriceWithPromotion()
+    {
+        if (ProductPromotion == null) 
+            return Product.Price.ToString("C");
+
+        var priceWithPromotion = Product.Price - (Product.Price * ((float)Promotion.DiscountPercentage / 100));
+        Console.WriteLine($"{priceWithPromotion} -- {Product.Price} -- {Promotion.DiscountPercentage}");
+        return priceWithPromotion.ToString("C");
+    }
 }
--- StudiBloc3_Mercadona.WebApp/Services/ApiProductService.cs
+++ StudiBloc3_Mercadona.WebApp/Services/ApiProductService.cs
@@ -14,9 +14,12 @@ public class ApiProductService(HttpClient httpClient)
         return products ?? Array.Empty<Product>();
     }
 
-    public async Task AddProductAsync(Product product)
+    public async Task<Product> AddProductAsync(Product product)
     {
         var response = await httpClient.PostAsJsonAsync("/api/Product/Product/Add", product);
         response.EnsureSuccessStatusCode();
+        
+        var addedProduct = await response.Content.ReadFromJsonAsync<Product>();
+        return addedProduct ?? new Product();
     }
 }
