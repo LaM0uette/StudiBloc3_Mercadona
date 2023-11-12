d456231b62c3c8a9d2880db761fdd6d91b1cda2a
LaM0uette <LaM0uette.dev@gmail.com>
2023-11-12 18:12:25 +0100

    basic design form

---
+++ StudiBloc3_Mercadona.App/Components/Pages/MenuBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using StudiBloc3_Mercadona.App.Services;
+
+namespace StudiBloc3_Mercadona.App.Components.Pages;
+
+public class MenuBase : ComponentBase
+{
+    [Inject] private NavigationManager NavigationManager { get; init; } = default!;
+    [Inject] private AuthenticationService authService { get; init; } = default!;
+    
+    protected string? Username;
+    protected string? Password;
+
+    protected async Task Login()
+    {
+        if (Username is null || Password is null) 
+        {
+            return;
+        }
+        
+        var result = await authService.Login(Username, Password);
+        if (!result)
+        {
+            return;
+        }
+        
+        NavigationManager.NavigateTo("/catalog");
+    }
+
+    protected async Task ContinueAsGuest()
+    {
+        await authService.Logout();
+        NavigationManager.NavigateTo("/catalog");
+    }
+}
